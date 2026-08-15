a20551d2ab5e771d67ef7aa8f30d43dad64c1dbd
dreizza007 <joseluann077@gmail.com>
2026-08-15 00:36:31 -0300

    Adiciona conexão com Supabase

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://utcjymncnkedfuwdjvmy.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_ObQzpf_Fwpku549zerg4Hw_3byeYAkE";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
